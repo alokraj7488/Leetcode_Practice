ff24eaa526074f2717a8fec46c559c9291c1a52d
Alok Raj <120704951+alokraj7488@users.noreply.github.com>
2026-06-01 23:37:37 +0530

    Time: 15 ms (98.22%) | Memory: 45.5 MB (64.85%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -3,18 +3,17 @@ class Solution {
         int n = nums.length;
         Arrays.sort(nums);
 
-        int closestSum = nums[0] + nums[1] + nums[2];
+        int maxDiff = Integer.MAX_VALUE;
+        int finalSum = 0;
         for(int i = 0; i < n-2; i++) {
-            if(i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-
             int left = i + 1;
             int right = n - 1;
             while(left < right) {
                 int total = nums[i] + nums[left] + nums[right];
-                if(Math.abs(target - total) < Math.abs(target - closestSum)) {
-                    closestSum = total;
+                int diff = Math.abs(target - total);
+                if(diff < maxDiff) {
+                    maxDiff = diff;
+                    finalSum = total;
                 }
                 if(total == target) {
                     return target;
@@ -27,6 +26,6 @@ class Solution {
                 }
             }
         }
-        return closestSum;
+        return finalSum;
     }
 }
