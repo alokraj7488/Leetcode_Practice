7fcae2c8adc37d45c98922bbc3f23b5e44324993
Alok Raj <120704951+alokraj7488@users.noreply.github.com>
2026-06-01 23:29:45 +0530

    Time: 19 ms (22.28%) | Memory: 45.5 MB (47.58%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -5,6 +5,10 @@ class Solution {
 
         int closestSum = nums[0] + nums[1] + nums[2];
         for(int i = 0; i < n-2; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
             int left = i + 1;
             int right = n - 1;
             while(left < right) {
