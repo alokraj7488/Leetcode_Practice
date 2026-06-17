66ed9f94955d53d191560c045fc3b204d4857fdc
Alok Raj <120704951+alokraj7488@users.noreply.github.com>
2026-06-18 00:36:57 +0530

    Time: 1256 ms (43.58%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT COALESCE(u.unique_id, null) AS unique_id, e.name AS name
+SELECT u.unique_id AS unique_id, e.name AS name
 FROM Employees e
 LEFT JOIN EmployeeUNI u
 ON e.id = u.id;
