bee095bd68ee2e77208cf153f67d50b83208a6b0
Alok Raj <120704951+alokraj7488@users.noreply.github.com>
2026-06-19 22:17:59 +0530

    Time: 245 ms (85.66%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+WITH CTE AS(
+SELECT machine_id, process_id, MAX(CASE WHEN activity_type = 'end' THEN timestamp END) AS end_time, 
+MAX(CASE WHEN activity_type = 'start' THEN timestamp END) AS start_time
+FROM Activity
+GROUP BY machine_id, process_id
+)
+SELECT machine_id, ROUND(AVG(end_time - start_time), 3) AS processing_time
+FROM CTE 
+GROUP BY machine_id;
